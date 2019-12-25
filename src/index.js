f756ba8c705c6e3ac34e3d60abdf3fe62977a493
Volodymyr <59233444+v08uka@users.noreply.github.com>
2019-12-25 21:29:23 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import Game from './сontainers/Game';
+import './index.css';
+import reducer from './reducers';
+import registerServiceWorker from './serviceWorker';
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+    reducer, /* preloadedState, */
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+/* eslint-enable */
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Game />
+    </Provider>,
+    document.getElementById('root'),
+);
+registerServiceWorker();
